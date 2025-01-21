c8ae763c9c0fe4313f62e76afd639678afa87380
hossenrabiul <robiulhossen0081@gmail.com>
2025-01-21 11:01:35 +0600

    database added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -95,18 +96,23 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 
 
 ...
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://mamar_bank_n4n0_user:L8aaGoR3wmYsh2mWKrq97Kjwpp66qAnv@dpg-cu7ij1ogph6c73bb3u1g-a.oregon-postgres.render.com/mamar_bank_n4n0',
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
